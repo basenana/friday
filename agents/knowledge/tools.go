6cbff9839941ece63e9ab6afd1aee3b0d9fe132b
hyponet <i@ihypo.net>
2025-12-18 21:52:56 +0800

    fix: k tools
    Signed-off-by: hyponet <i@ihypo.net>

--- agents/knowledge/tools.go
+++ agents/knowledge/tools.go
@@ -1,11 +1,1 @@
 package knowledge
-
-import (
-	"context"
-	"fmt"
-	"github.com/basenana/friday/types"
-	"strconv"
-
-	"github.com/basenana/friday/storehouse"
-	"github.com/basenana/friday/tools"
-)
